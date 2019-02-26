9763caf2a337d2df46d11d4f40ecfd40a0cccbf9
Piotr Paszko <piter9p@interia.pl>
2019-02-26 16:47:59 +0100

    Implemented DBContext, enabled manual migrations and added Article class.

---
+++ WikiHash/WikiHash/DAL/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using WikiHash.Models;
+
+namespace WikiHash.DAL
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public DbSet<Models.Articles.Article> Articles { get; set; }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
---
+++ WikiHash/WikiHash/Models/Articles/Article.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WikiHash.Models.Articles
+{
+    public class Article
+    {
+        [Key]
+        public int ArticleId { get; set; }
+
+        [MaxLength(128)]
+        public string Title { get; set; }
+    }
+}
