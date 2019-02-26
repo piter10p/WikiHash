a34e2c2486732de4204917f31e5f1226ff9d9b41
Piotr Paszko <piter9p@interia.pl>
2019-02-26 16:00:22 +0100

    Created VS Project.

---
+++ WikiHash/WikiHash/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WikiHash.Startup))]
+namespace WikiHash
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
