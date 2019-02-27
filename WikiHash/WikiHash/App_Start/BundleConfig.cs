26b0a870387e6fb777e5bb2a473614c425f38f16
Piotr Paszko <piter9p@interia.pl>
2019-02-27 15:09:11 +0100

    Updated Article Read page layout.

--- WikiHash/WikiHash/App_Start/BundleConfig.cs
+++ WikiHash/WikiHash/App_Start/BundleConfig.cs
@@ -8,11 +8,10 @@ namespace WikiHash
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+            bundles.Add(new ScriptBundle("~/bundles/js").Include(
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery.validate*",
+                        "~/Scripts/script.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
---
+++ WikiHash/WikiHash/Scripts/script.js
@@ -0,0 +1,12 @@
+﻿//Add slide down effect to links with data-slide=true attributes
+$('a[data-slide=true]').on('click', function (event) {
+    event.preventDefault();
+    var targetOffset = $(this.hash).offset().top;
+    var currentOffset = window.pageYOffset;
+
+    if (currentOffset > targetOffset)
+        $('html,body').animate({ scrollTop: targetOffset - 75 }, 500);
+    else
+        $('html,body').animate({ scrollTop: targetOffset - 75 }, 500);
+
+});
