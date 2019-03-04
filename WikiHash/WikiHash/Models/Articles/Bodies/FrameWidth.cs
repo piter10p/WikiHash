29e0fccdfd2e22ba5d8b234e899e44edc6cceff2
Piotr Paszko <piter9p@interia.pl>
2019-03-04 16:49:43 +0100

    Added FrameWidthParser.ToString method.

--- WikiHash/WikiHash/Models/Articles/Bodies/FrameWidth.cs
+++ WikiHash/WikiHash/Models/Articles/Bodies/FrameWidth.cs
@@ -12,28 +12,59 @@ namespace WikiHash.Models.Articles.Bodies
 
     public static class FrameWidthParser
     {
+        const string W1String = "1";
+        const string W2String = "2";
+        const string W3String = "3";
+        const string W4String = "4";
+        const string W5String = "5";
+        const string W6String = "6";
+        const string WAutoString = "auto";
+
         public static FrameWidth Parse(string text)
         {
             switch(text)
             {
-                case "1":
+                case W1String:
                     return FrameWidth.W1;
-                case "2":
+                case W2String:
                     return FrameWidth.W2;
-                case "3":
+                case W3String:
                     return FrameWidth.W3;
-                case "4":
+                case W4String:
                     return FrameWidth.W4;
-                case "5":
+                case W5String:
                     return FrameWidth.W5;
-                case "6":
+                case W6String:
                     return FrameWidth.W6;
-                case "auto":
+                case WAutoString:
                     return FrameWidth.AUTO;
 
             }
 
             throw new Exception("Failed to parse Frame Width string.");
+        }
+
+        public static string ToString(FrameWidth width)
+        {
+            switch(width)
+            {
+                case FrameWidth.W1:
+                    return W1String;
+                case FrameWidth.W2:
+                    return W2String;
+                case FrameWidth.W3:
+                    return W3String;
+                case FrameWidth.W4:
+                    return W4String;
+                case FrameWidth.W5:
+                    return W5String;
+                case FrameWidth.W6:
+                    return W6String;
+                case FrameWidth.AUTO:;
+                    return WAutoString;
+            }
+
+            throw new Exception("Failed to convert from Frame Width to string.");
         }
 
         public static string ToBootstrapString(FrameWidth frameWidth)
