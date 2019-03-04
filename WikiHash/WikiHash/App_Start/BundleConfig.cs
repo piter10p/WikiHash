b8231291142b0beb5dfa79e56776491828cc13a3
Piotr Paszko <piter9p@interia.pl>
2019-03-04 14:17:25 +0100

    Installed font awesome.

--- WikiHash/WikiHash/App_Start/BundleConfig.cs
+++ WikiHash/WikiHash/App_Start/BundleConfig.cs
@@ -19,11 +19,12 @@ namespace WikiHash
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/bootstrap.min.css",
+                      "~/Content/site.css",
+                      "~/font-awesome.min.css"));
         }
     }
 }
