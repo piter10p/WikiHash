932c2c2753c80b74bad6b4b40bdf4b00c0c79adc
Piotr Paszko <piter9p@interia.pl>
2019-03-11 18:24:14 +0100

    Implemented admin role.

--- WikiHash/WikiHash/Migrations/Configuration.cs
+++ WikiHash/WikiHash/Migrations/Configuration.cs
@@ -4,6 +4,9 @@ namespace WikiHash.Migrations
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using WikiHash.Models;
 
     internal sealed class Configuration : DbMigrationsConfiguration<WikiHash.DAL.ApplicationDbContext>
     {
@@ -14,9 +17,19 @@ namespace WikiHash.Migrations
 
         protected override void Seed(DAL.ApplicationDbContext context)
         {
+            /*if (!System.Diagnostics.Debugger.IsAttached)
+                System.Diagnostics.Debugger.Launch();*/
+
+            //Default
+            GenerateRoles(context);
+            GenerateUsers(context);
+
+            //Test
             var testCategory = GenerateTestCategories(context);
             GenerateTestArticles(context, testCategory);
             GenerateTestMedias(context, testCategory);
+
+            context.SaveChanges();
             //  This method will be called after migrating to the latest version.
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
@@ -25,34 +38,57 @@ namespace WikiHash.Migrations
 
         private void GenerateTestArticles(DAL.ApplicationDbContext context, Models.Category testCategory)
         {
-            if(context.Articles.Count() == 0)
-            {
-                context.Articles.Add(new Models.Articles.Article() { Title = "Test Article", Category = testCategory });
-                context.Articles.AddOrUpdate();
-            }
+            if (!context.Articles.Any(a => a.Title == "Test Article"))
+                context.Articles.AddOrUpdate(new Models.Articles.Article() { Title = "Test Article", Category = testCategory });
         }
 
         private void GenerateTestMedias(DAL.ApplicationDbContext context, Models.Category testCategory)
         {
-            if (context.Medias.Count() == 0)
-            {
+            if(!context.Medias.Any( m => m.Title == "Test Media"))
                 context.Medias.Add(new Models.Medias.Media() { Title = "Test Media", Description = "Some test description of media", FileName = "test-media.jpg", Category = testCategory });
+
+            if (!context.Medias.Any(m => m.Title == "Nyan Cat"))
                 context.Medias.Add(new Models.Medias.Media() { Title = "Nyan Cat", Description = "Get Nyaned! A video media example", FileName = "nyan cat.mp4" });
+
+            if (!context.Medias.Any(m => m.Title == "Programming"))
                 context.Medias.Add(new Models.Medias.Media() { Title = "Programming", Description = "A gif media example", FileName = "programming.gif" });
-                context.Medias.AddOrUpdate();
-            }
         }
 
-        private Models.Category GenerateTestCategories(DAL.ApplicationDbContext context)
+        private Category GenerateTestCategories(DAL.ApplicationDbContext context)
         {
-            if(context.Categories.Count() == 0)
+            if (!context.Categories.Any(c => c.CategoryName == "Test category"))
             {
-                var category = context.Categories.Add(new Models.Category() { CategoryName = "Test category" });
+                var category = context.Categories.Add(new Category() { CategoryName = "Test category" });
                 context.Categories.AddOrUpdate();
                 return category;
             }
 
-            return context.Categories.First();
+            var query = from c in context.Categories where c.CategoryName == "Test category" select c;
+            return query.First();
+        }
+
+        private void GenerateRoles(DAL.ApplicationDbContext context)
+        {
+            if (!context.Roles.Any(r => r.Name == RoleNames.Admin))
+            {
+                var role = new IdentityRole { Name = RoleNames.Admin };
+                var store = new RoleStore<IdentityRole>(context);
+                var manager = new RoleManager<IdentityRole>(store);
+                manager.Create(role);
+            }
+        }
+
+        private void GenerateUsers(DAL.ApplicationDbContext context)
+        {
+            if (!context.Users.Any(u => u.UserName == "admin@admin.admin"))
+            {
+                var store = new UserStore<ApplicationUser>(context);
+                var manager = new UserManager<ApplicationUser>(store);
+                var user = new ApplicationUser { UserName = "admin@admin.admin" };
+
+                manager.Create(user, "!Pass123");
+                manager.AddToRole(user.Id, RoleNames.Admin);
+            }
         }
     }
 }
